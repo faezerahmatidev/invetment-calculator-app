01361a09d73242a30fd449d53d73cac4a598da3a
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-06 19:04:30 +0330

    feat(investment-results): implement results display and calculation logic
    - Add InvestmentResultsComponent and integrate it in AppComponent
    - Implement logic to calculate and display investment results
    - Update HTML and CSS to show the results table
    - Add input and output handling for investment data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { type InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
   onCalculateInvestmentResults(data: InvestmentInput) {
     const annualData = [];
     let investmentValue = data.initialInvestment;
@@ -31,8 +40,6 @@ export class AppComponent {
           data.initialInvestment + data.annualInvestment * year,
       });
     }
-
-    console.log(annualData);
-    return annualData;
+    this.resultsData = annualData;
   }
 }
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input } from '@angular/core';
-
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  @Input() investmentResults: {
+  @Input() results?: {
     year: number;
-    yearlyInterest: number;
-    savingsEndOfYear: number;
-    totalInterestGained: number;
-    totalInvestedCapital: number;
-  }[] = [];
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
 }
